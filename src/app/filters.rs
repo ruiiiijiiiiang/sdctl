eaa06d0c1254a24f1c45313ad2ba17b35867c34b
Rui Jiang <me@ruijiang.me>
2026-05-23 16:07:57 -0500

    fixed restore_selection behavior while filtering

--- src/app/filters.rs
+++ src/app/filters.rs
@@ -171,13 +171,13 @@ impl App {
     }
 
     pub fn update_filter(&mut self) {
-        if self.search.query.is_empty() {
-            let selected_unit_key = if self.unit_list.selected_key == UnitSelectionKey::default() {
-                None
-            } else {
-                Some(self.unit_list.selected_key.clone())
-            };
+        let selected_unit_key = if self.unit_list.selected_key == UnitSelectionKey::default() {
+            None
+        } else {
+            Some(self.unit_list.selected_key.clone())
+        };
 
+        if self.search.query.is_empty() {
             self.unit_list.filtered_indices = self
                 .unit_list
                 .units
@@ -189,7 +189,6 @@ impl App {
             self.unit_list
                 .filtered_indices
                 .sort_by_key(|&index| self.unit_list.units[index].name.to_ascii_lowercase());
-            self.restore_selection(selected_unit_key.as_ref());
         } else {
             let mut scored: Vec<(usize, u32)> = self
                 .unit_list
@@ -208,12 +207,8 @@ impl App {
                 })
             });
             self.unit_list.filtered_indices = scored.into_iter().map(|(index, _)| index).collect();
-            if !self.unit_list.filtered_indices.is_empty() {
-                self.unit_list.select_index(Some(0));
-            } else {
-                self.unit_list.select_index(None);
-            }
         }
+        self.restore_selection(selected_unit_key.as_ref());
     }
 
     pub fn filter_summary(&self, menu: FilterMenu) -> String {
